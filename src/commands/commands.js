7a920c1b54d428fdbc9fddfc849b18ba425ffe9b
Charlie Cheever <ccheever@gmail.com>
2015-05-08 00:00:18 -0700

    Temporarily removing adduser/login commands

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -2,8 +2,8 @@ var commands = {
   serve: require('./serve'),
   url: require('./url'),
   bundle: require('./bundle'),
-  adduser: require('./adduser'),
-  login: require('./login'),
+  //adduser: require('./adduser'),
+  //login: require('./login'),
   new: require('./new'),
 };
 
